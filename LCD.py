d769623478089706db3d902927f5c2878d272843
tenzin1308 <tenztashi8@gmail.com>
2020-07-15 15:45:28 -0400

    07.15 LCD Clock

--- LCD.py
+++ LCD.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
-import LCD1602
+import LCD1602 as lcd
+from datetime import datetime
 import time
 
 def setup():
-	LCD1602.init(0x27,1) #LCD Address and Background Color
+	lcd.init(0x27,1) #LCD Address and Background Color
 
 def exit():
-	LCD1602.clear()
+	lcd.clear()
+	bye_msg = 'Shutting Down In'
+	lcd.write(0,0,bye_msg)
+	lcd.write(1,7,str(3))
+	time.sleep(1)
+	lcd.write(1,7,str(2))
+	time.sleep(1)
+	lcd.write(1,7,str(1))
+	time.sleep(1)
+
+	lcd.clear()
 
 def start():
 	welcome_string='Clock Hour'
-	LCD1602.write(0,0,welcome_string)
+	lcd.write(0,0,welcome_string)
 	while(True):
-		LCD1602.write(1,1,str(int(time.time()*1000)))
+		lcd.write(1,1,datetime.now().strftime('%H:%M:%S:%f'))
 		time.sleep(.05)
 
 if __name__ == '__main__':
